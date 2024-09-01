765a6dce36864ba69aa30e6262350b6c1d708c05
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-09-01 17:26:40 +0200

    added ui module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.nowinandroid.android.library)
+    alias(libs.plugins.nowinandroid.android.library.compose)
+    alias(libs.plugins.nowinandroid.android.library.jacoco)
+    alias(libs.plugins.roborazzi)
+}
+
+android {
+    namespace = "com.example.ui"
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    api(libs.androidx.metrics)
+//    api(projects.core.analytics)
+//    api(projects.core.designsystem)
+    api(projects.core.model)
+
+    implementation(libs.androidx.browser)
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+
+    androidTestImplementation(libs.bundles.androidx.compose.ui.test)
+//    androidTestImplementation(projects.core.testing)
+
+    lintPublish(projects.core)
+
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.foundation.layout)
+    api(libs.androidx.compose.material.iconsExtended)
+    api(libs.androidx.compose.material3)
+    api(libs.androidx.compose.material3.adaptive)
+    api(libs.androidx.compose.material3.navigationSuite)
+    api(libs.androidx.compose.runtime)
+    api(libs.androidx.compose.ui.util)
+
+    implementation(libs.coil.kt.compose)
+
+    testImplementation(libs.androidx.compose.ui.test)
+    testImplementation(libs.androidx.compose.ui.testManifest)
+
+    testImplementation(libs.hilt.android.testing)
+    testImplementation(libs.robolectric)
+//    testImplementation(projects.core.screenshotTesting)
+
+    androidTestImplementation(libs.bundles.androidx.compose.ui.test)
+}
