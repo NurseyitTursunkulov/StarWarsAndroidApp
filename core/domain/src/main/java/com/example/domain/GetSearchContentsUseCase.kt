e2ce60a1f5b83825ecdc18e0af04b2f680d4260a
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-09-01 21:06:30 +0200

    added for you screen

---
+++ core/domain/src/main/java/com/example/domain/GetSearchContentsUseCase.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.domain
+
+
+import com.example.data.repository.SearchContentsRepository
+import com.example.data.repository.UserDataRepository
+import com.example.model.data.FollowableTopic
+import com.example.model.data.SearchResult
+import com.example.model.data.UserData
+import com.example.model.data.UserNewsResource
+import com.example.model.data.UserSearchResult
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import javax.inject.Inject
+
+/**
+ * A use case which returns the searched contents matched with the search query.
+ */
+class GetSearchContentsUseCase @Inject constructor(
+    private val searchContentsRepository: SearchContentsRepository,
+    private val userDataRepository: UserDataRepository,
+) {
+
+    operator fun invoke(
+        searchQuery: String,
+    ): Flow<UserSearchResult> =
+        searchContentsRepository.searchContents(searchQuery)
+            .mapToUserSearchResult(userDataRepository.userData)
+}
+
+private fun Flow<SearchResult>.mapToUserSearchResult(userDataStream: Flow<UserData>): Flow<UserSearchResult> =
+    combine(userDataStream) { searchResult, userData ->
+        UserSearchResult(
+            topics = searchResult.topics.map { topic ->
+                FollowableTopic(
+                    topic = topic,
+                    isFollowed = topic.id in userData.followedTopics,
+                )
+            },
+            newsResources = searchResult.newsResources.map { news ->
+                UserNewsResource(
+                    newsResource = news,
+                    userData = userData,
+                )
+            },
+        )
+    }
---
+++ feature/foryou/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    alias(libs.plugins.nowinandroid.android.feature)
+    alias(libs.plugins.nowinandroid.android.library.compose)
+    alias(libs.plugins.nowinandroid.android.library.jacoco)
+    alias(libs.plugins.roborazzi)
+}
+
+android {
+    namespace = "com.example.foryou"
+}
+
+dependencies {
+    implementation(libs.accompanist.permissions)
+    implementation(projects.core.data)
+    implementation(projects.core.domain)
+
+    testImplementation(libs.hilt.android.testing)
+    testImplementation(libs.robolectric)
+//    testImplementation(projects.core.testing)
+//    testDemoImplementation(projects.core.screenshotTesting)
+
+    androidTestImplementation(libs.bundles.androidx.compose.ui.test)
+//    androidTestImplementation(projects.core.testing)
+}
