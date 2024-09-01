b945a495bb03a04167cf838779291acf283c695e
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-09-01 17:54:08 +0200

    added Bookmarks module

---
+++ feature/bookmarks/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.nowinandroid.android.feature)
+    alias(libs.plugins.nowinandroid.android.library.compose)
+    alias(libs.plugins.nowinandroid.android.library.jacoco)
+}
+
+android {
+    namespace = "com.example.bookmarks"
+}
+
+dependencies {
+    implementation(projects.core.data)
+
+//    testImplementation(projects.core.testing)
+
+    androidTestImplementation(libs.bundles.androidx.compose.ui.test)
+//    androidTestImplementation(projects.core.testing)
+}
