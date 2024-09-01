7b3efe5ff79a8c62c97791dfbc7ed0e11725c968
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-09-01 21:25:53 +0200

    added search module

---
+++ feature/search/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.nowinandroid.android.feature)
+    alias(libs.plugins.nowinandroid.android.library.compose)
+    alias(libs.plugins.nowinandroid.android.library.jacoco)
+}
+
+android {
+    namespace = "com.example.search"
+}
+
+dependencies {
+    implementation(projects.core.data)
+    implementation(projects.core.domain)
+    implementation(projects.core.ui)
+
+//    testImplementation(projects.core.testing)
+
+    androidTestImplementation(libs.bundles.androidx.compose.ui.test)
+//    androidTestImplementation(projects.core.testing)
+}
