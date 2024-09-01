a3c0b0c017be4cfa7eae42c357c8cab1c7cb058f
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-09-01 08:42:52 +0200

    added core and common module

---
+++ app/src/main/java/com/example/myapplication/Application.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.myapplication
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+/**
+ * [Application] class for NiA
+ */
+//@HiltAndroidApp
+class Application : Application() {
+//    @Inject
+//    lateinit var imageLoader: dagger.Lazy<ImageLoader>
+//    //todo add synchronization
+//
+//    override fun newImageLoader(): ImageLoader = imageLoader.get()
+}
