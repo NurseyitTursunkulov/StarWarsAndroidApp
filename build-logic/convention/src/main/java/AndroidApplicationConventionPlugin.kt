cab2502f5674416322bd0f3e33b66083d897ffa7
Buukhalai Mamadaminova <buukhalai.mamadaminova.mfn@fh-zwickau.de>
2024-09-09 09:08:16 +0200

    removed Badging.kt

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -16,15 +16,12 @@
 
 import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.variant.ApplicationAndroidComponentsExtension
-import com.android.build.gradle.BaseExtension
-import com.example.convention.configureBadgingTasks
 import com.example.convention.configureGradleManagedDevices
 import com.example.convention.configureKotlinAndroid
 import com.example.convention.configurePrintApksTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -45,7 +42,6 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             }
             extensions.configure<ApplicationAndroidComponentsExtension> {
                 configurePrintApksTask(this)
-                configureBadgingTasks(extensions.getByType<BaseExtension>(), this)
             }
         }
     }
